0d3723c1b1c23dda9550894f55e12ced5e7fc236
Grady Wetherbee <gradywetherbee2021@u.northwestern.edu>
2019-05-15 00:04:04 -0500

    changed time format on popup

--- js/map.js
+++ js/map.js
@@ -16,8 +16,8 @@ for (var eventID in eventManifest.Events) {
 
   marker.addListener('click', function(e) {
     var title = eventManifest.Events[this.eventID].title;
-    var startTime = "Start time: " + eventManifest.Events[this.eventID].startTime;
-    var endTime = "End time: " + eventManifest.Events[this.eventID].endTime;
+    var startTime = eventManifest.Events[this.eventID].startTime;
+    var endTime = eventManifest.Events[this.eventID].endTime;
     var description = eventManifest.Events[this.eventID].description;
 
     closeEventPopup();
@@ -28,8 +28,7 @@ for (var eventID in eventManifest.Events) {
             <i class='material-icons float-right' onclick='closeEventPopup()'>close</i>\
             <h3>" + title + "</h3> \
             <h4>ROOM</h4>\
-            <h4> Start time: " + startTime + "</h4> \
-            <h4> End time: " + endTime + "</h4> \
+            <h4> Time: " + startTime + " - " + endTime + "</h4> \
             <h4>Salad, Pizza, Middle-Eastern </h4> \
             <p>" + description + "</p>";
     document.body.append(eventPopup);
