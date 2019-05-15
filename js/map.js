a225ccef7f2d5f3b61dd388029a7d2b74b650be2
Aaron Nelson <AaronNelson@u.northwestern.edu>
2019-05-14 22:14:38 -0500

    entry modification js

--- js/map.js
+++ js/map.js
@@ -5,16 +5,24 @@ var map = new google.maps.Map(document.getElementById('map'), {
 
 for (var eventID in eventManifest.Events) {
   var marker = new google.maps.Marker({
-      position: {lat: 42.057656, lng: -87.67428},
+      position: {lat: eventManifest.Events[eventID].lat,
+        lng: eventManifest.Events[eventID].lng},
       map: map,
-      title: 'Click to zoom'
+      title: 'Click to zoom',
+      eventID: eventID
   });
 
   marker.addListener('click', function(e) {
-      displayEvent(eventID);
-  });
-};
+    var title = document.getElementById("event-popup-title");
+    var startTime = document.getElementById("event-popup-start-time");
+    var endTime = document.getElementById("event-popup-end-time");
+    var description = document.getElementById("event-popup-description");
 
-function displayEvent(eventID) {
-  console.log(eventID);
+    title.innerHTML = eventManifest.Events[this.eventID].title;
+    startTime.innerHTML = "Start time: " + eventManifest.Events[this.eventID].startTime;
+    endTime.innerHTML = "End time: " + eventManifest.Events[this.eventID].endTime;
+    description.innerHTML = eventManifest.Events[this.eventID].description;
+    //event-popup-(json-name)
+  });
+
 };
