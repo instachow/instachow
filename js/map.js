5946b911398a238125147822354bcaea54057e5e
Grady Wetherbee <gradywetherbee2021@u.northwestern.edu>
2019-05-15 00:07:15 -0500

    add room from json to popup

--- js/map.js
+++ js/map.js
@@ -15,10 +15,13 @@ for (var eventID in eventManifest.Events) {
   });
 
   marker.addListener('click', function(e) {
-    var title = eventManifest.Events[this.eventID].title;
-    var startTime = eventManifest.Events[this.eventID].startTime;
-    var endTime = eventManifest.Events[this.eventID].endTime;
-    var description = eventManifest.Events[this.eventID].description;
+    var event = eventManifest.Events[this.eventID];
+    var title = event.title;
+    var startTime = event.startTime;
+    var endTime = event.endTime;
+    var description = event.description;
+    var description = event.description;
+    var room = event.room;
 
     closeEventPopup();
     var eventPopup = document.createElement("div");
@@ -27,7 +30,7 @@ for (var eventID in eventManifest.Events) {
     eventPopup.innerHTML += "\
             <i class='material-icons float-right' onclick='closeEventPopup()'>close</i>\
             <h3>" + title + "</h3> \
-            <h4>ROOM</h4>\
+            <h4>" + room + "</h4>\
             <h4> Time: " + startTime + " - " + endTime + "</h4> \
             <h4>Salad, Pizza, Middle-Eastern </h4> \
             <p>" + description + "</p>";
