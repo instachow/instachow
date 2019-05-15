823ecf7168b172685e77c6d051451d341ee5b90b
Calypso McDonnell <calypsomcdonnell2021@u.northwestern.edu>
2019-05-14 22:14:01 -0500

    more

--- js/list.js
+++ js/list.js
@@ -1,39 +1,79 @@
-var listOfEvents = require("data.json");
+var listOfEvents = EventList.Events;
 
 function buildEventList() {
     var table = document.getElementById("album-list");
+    console.log(table);
     if (table != null) {
-        for (var i = 0; i < listOfEvents.length; i++) {
+        console.log(listOfEvents);
+        for (var i in listOfEvents) {
             var event = document.createElement('div');
             event.classList.add('event-container');
 
             var start = document.createElement('p');
             start.classList.add('event-start');
-            start.innerHTML = listOfEvents[i].startTime;
+            var st = i.startTime;
+            console.log(i);
 
+            start.innerHTML = formatDate(st);
+            event.appendChild(start);
+            console.log(start, "hi");
             var end = document.createElement('p');
             end.classList.add('event-end');
-            end.innerHTML = listOfEvents[i].endTime;
+            et = i.endTime;
+            end.innerHTML = formatDate(et);
+            event.appendChild(end);
 
-            var title = document.createElement('p');
-            title.classList.add('event-title');
-            title.innerHTML = listOfEvents[i].title;
+            var categories = document.createElement('p');
+            categories.classList.add('event-categories');
+            var cat = i.categories;
+            var string;
+            for (var j = 0; j < cat.length; i++) {
+                if (j != cat.length - 1) {
+                    string += cat[j] + ", ";
+                } else {
+                    string += cat[j];
+                }
+            }
+            categories.innerHTML = string;
+            event.appendChild(categories);
 
-            var title = document.createElement('p');
-            title.classList.add('event-title');
-            title.innerHTML = listOfEvents[i].title;
-
-            var title = document.createElement('p');
-            title.classList.add('event-title');
-            title.innerHTML = listOfEvents[i].title;
-
-            var title = document.createElement('p');
-            title.classList.add('event-title');
-            title.innerHTML = listOfEvents[i].title;
-
-            var title = document.createElement('p');
-            title.classList.add('event-title');
-            title.innerHTML = listOfEvents[i].title;
+            var description = document.createElement('p');
+            description.classList.add('event-description');
+            description.innerHTML = i.description;
+            event.appendChild(description);
+            table.appendChild(event);
         }
     }
+}
+
+
+function formatDate(date) {
+    var d = new Date(date);
+    var hh = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    var dd = "AM";
+    var h = hh;
+    if (h >= 12) {
+        h = hh - 12;
+        dd = "PM";
+    }
+    if (h == 0) {
+        h = 12;
+    }
+    m = m < 10 ? "0" + m : m;
+
+    s = s < 10 ? "0" + s : s;
+
+    /* if you want 2 digit hours:
+    h = h<10?"0"+h:h; */
+
+    var pattern = new RegExp("0?" + hh + ":" + m + ":" + s);
+
+    var replacement = h + ":" + m;
+    /* if you want to add seconds
+    replacement += ":"+s;  */
+    replacement += " " + dd;
+
+    return date.replace(pattern, replacement);
 }
