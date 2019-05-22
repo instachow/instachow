c3347a6d22cff7aa36ece02d1e694a0b1ac39671
Grady Wetherbee <gradywetherbee2021@u.northwestern.edu>
2019-05-21 20:18:27 -0500

    open filter popup from logo

--- js/events.js
+++ js/events.js
@@ -7,23 +7,26 @@ function closeEventPopup() {
 
 var currentFilters = [];
 
-var filterPopup = document.createElement("div");
-filterPopup.setAttribute("id", "filter-popup");
-filterPopup.setAttribute("class", "event-popup icon pad");
-filterPopup.innerHTML += "\
-        <i class='material-icons float-right' onclick='closeFilterPopup()'>close</i>\
-        <h3> Filters </h3>";
-var filters = availableFilters.Filters;
-console.log(filters);
-    
-for(food in filters){
-  var filterItem = document.createElement("h4");
-  filterItem.setAttribute("id", food);
-  filterItem.setAttribute("onclick", addToFilter(food));
-  filterItem.innerHTML +=  food;
-  filterPopup.append(filterItem);
+function openFilterPopup(){
+  var filterPopup = document.createElement("div");
+  filterPopup.setAttribute("id", "filter-popup");
+  filterPopup.setAttribute("class", "event-popup icon pad");
+  filterPopup.innerHTML += "\
+          <i class='material-icons float-right' onclick='closeFilterPopup()'>close</i>\
+          <h3> Filters </h3>";
+  var filters = availableFilters.Filters;
+  console.log(filters);
+      
+  for(food in filters){
+    var filterItem = document.createElement("h4");
+    filterItem.setAttribute("id", food);
+    filterItem.setAttribute("onclick", "addToFilter(" + food + ")");
+    filterItem.innerHTML +=  food;
+    filterPopup.append(filterItem);
+  }
+  document.body.append(filterPopup);
 }
-document.body.append(filterPopup);
+
 
 function closeFilterPopup() {
   var filterPopup = document.getElementById('filter-popup');
