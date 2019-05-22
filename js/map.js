6d89c1d0c621ce5dddc531ffb813fcadaa921347
Calypso McDonnell <calypsomcdonnell2021@u.northwestern.edu>
2019-05-21 21:36:39 -0500

    updates

--- js/map.js
+++ js/map.js
@@ -1,59 +1,78 @@
 var map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 42.057656, lng: -87.67428},
+  center: {
+    lat: 42.057656,
+    lng: -87.67428
+  },
   zoom: 15,
   disableDefaultUI: true,
   clickableIcons: false,
-  styles: [
-    {
+  styles: [{
       "featureType": "poi",
-      "stylers": [
-        {
-          "visibility": "simplified"
-        }
-      ]
+      "stylers": [{
+        "visibility": "simplified"
+      }]
     },
     {
       "featureType": "poi",
       "elementType": "labels",
-      "stylers": [
-        {
-          "weight": 1.5
-        }
-      ]
+      "stylers": [{
+        "weight": 1.5
+      }]
     },
     {
       "featureType": "poi.business",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
       "featureType": "poi.park",
       "elementType": "labels.text",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      "stylers": [{
+        "visibility": "off"
+      }]
     }
   ]
 });
 
+
 console.log(eventManifest.Events);
-for (var eventID in eventManifest.Events) {
-  console.log(Math.floor(Math.random()*15));
-  var marker = new google.maps.Marker({
-      position: {lat: eventManifest.Events[eventID].lat,
-        lng: eventManifest.Events[eventID].lng},
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    var marker = new google.maps.Marker({
+      position: {
+        lat: pos.lat,
+        lng: pos.lng
+      },
       map: map,
-      title: 'Click to zoom',
-      eventID: eventID,
-      icon: 'assets/' + Math.floor(Math.random()*16) + '.png'
+      title: 'Current location',
+      eventID: -1,
+      icon: 'assets/location.png',
+    })
+    console.log(marker);
+    map.setCenter(pos);
+    map.setZoom(16);
+  })
+}
+for (var eventID in eventManifest.Events) {
+  console.log(eventID.icon);
+  var marker = new google.maps.Marker({
+    position: {
+      lat: eventManifest.Events[eventID].lat,
+      lng: eventManifest.Events[eventID].lng
+    },
+    map: map,
+    title: 'Click to zoom',
+    eventID: eventID,
+    icon: 'assets/' + eventManifest.Events[eventID].icon
+    //icon: 'assets/' + Math.floor(Math.random() * 16) + '.png'
   });
 
-  marker.addListener('click', function(e) {
+  marker.addListener('click', function (e) {
     //close old popup if there is one
     closeFilterPopup();
     closeEventPopup();
@@ -66,7 +85,7 @@ for (var eventID in eventManifest.Events) {
     var description = event.description;
     var food = event.foodCategories;
     var room = event.room;
-    
+
     var eventPopup = document.createElement("div");
     eventPopup.setAttribute("id", "event-popup");
     eventPopup.setAttribute("class", "event-popup details-popup icon pad");
@@ -80,26 +99,29 @@ for (var eventID in eventManifest.Events) {
             <i class='material-icons float-right'>directions</i>";
     document.body.append(eventPopup);
 
-  //resize map and center on the point clicked
+    //resize map and center on the point clicked
     //account for popup on left side
     lat = event.lat;
     lng = event.lng;
-    if(window.innerWidth > 799){
-      if(lng > 0)
-      lng = event.lng + 0.004;
+    if (window.innerWidth > 799) {
+      if (lng > 0)
+        lng = event.lng + 0.004;
       else
-      lng = event.lng - 0.004;
+        lng = event.lng - 0.004;
     }
     //account for popup on bottom
     else {
-      if(lat > 0)
-      lat = event.lat  - 0.002;
+      if (lat > 0)
+        lat = event.lat - 0.002;
       else
-      lat = event.lat  + 0.002;
+        lat = event.lat + 0.002;
     }
-    
+
     map.setZoom(16);
-    map.panTo({lat, lng});
+    map.panTo({
+      lat,
+      lng
+    });
     // marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
   });
 
